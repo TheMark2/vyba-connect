38f1083e51edf4c9ea79b44f69af8dd93c68e80d
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-03-15 16:59:04 +0100

    s

--- src/components/ArtistProfileCard.tsx
+++ src/components/ArtistProfileCard.tsx
@@ -57,7 +57,7 @@ const ArtistProfileCard = ({
         <div
           className="w-full h-full transition-transform duration-300"
           style={{
-            transform: isHovered ? 'scale(1.05)' : 'scale(1)',
+            transform: isHovered ? 'scale(1.0)' : 'scale(1)',
           }}
         >
           <img
