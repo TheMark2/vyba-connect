31cd0a33666ef122bc08fd3dfd7de47ba36b26ac
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-03-15 20:32:20 +0100

    s

--- src/components/HelpSection.tsx
+++ src/components/HelpSection.tsx
@@ -17,7 +17,7 @@ const HelpSection = ({ className }: HelpSectionProps) => {
   
   return (
     <section className={cn("px-12 md:px-14 lg:px-16", className)}>
-      <div className="py-16 bg-[#F5F1EB]">
+      <div className="py-16 bg-[#F5F1EB] rounded-3xl">
         <div className="flex flex-col md:flex-row items-center justify-between">
           <h2 className="text-3xl md:text-4xl lg:text-5xl font-black mb-6 md:mb-0">
             ¿Tienes dudas?
