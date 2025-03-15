ff15efcec39801f5d39fb9c17180737f7a2eab4c
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-03-15 11:03:35 +0100

    ws

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -21,7 +21,7 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-6 py-3",
+        default: "px-6 py-3",
         sm: "h-9 rounded-full px-3",
         lg: "h-11 rounded-full px-8",
         icon: "h-10 w-10",
