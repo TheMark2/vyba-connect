b1cd51165fac30b5d8c565f653fcd1a07c8c663d
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-03-15 20:28:29 +0100

    s

--- src/components/StatsSummary.tsx
+++ src/components/StatsSummary.tsx
@@ -10,7 +10,7 @@ interface StatsItemProps {
 const StatsItem = ({ count, label, className }: StatsItemProps) => {
   return (
     <div className={cn("flex flex-col items-center", className)}>
-      <span className="text-5xl md:text-6xl lg:text-7xl font-black">{count}</span>
+      <span className="text-3xl md:text-6xl lg:text-7xl font-black">{count}</span>
       <span className="text-lg md:text-xl font-medium mt-2">{label}</span>
     </div>
   );
@@ -23,7 +23,7 @@ interface StatsSummaryProps {
 const StatsSummary = ({ className }: StatsSummaryProps) => {
   return (
     <section className={cn("px-12 md:px-14 lg:px-16", className)}>
-      <div className="bg-[#F1F0FB] rounded-3xl">
+      <div className="bg-[#F1F0FB] rounded-3xl py-16">
         <div className="container mx-auto px-6 md:px-10">
           <div className="flex flex-wrap justify-center gap-10 md:gap-20 lg:gap-32">
             <StatsItem count="1.000" label="Artistas registrados" />
