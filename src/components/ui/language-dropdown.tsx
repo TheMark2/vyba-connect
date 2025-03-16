202a3272c0201438321739c2ee4e938226cd7d2a
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-03-16 20:08:32 +0100

    s

--- src/components/ui/language-dropdown.tsx
+++ src/components/ui/language-dropdown.tsx
@@ -42,7 +42,7 @@ export function LanguageDropdown({ className }: LanguageDropdownProps) {
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent 
-        className="min-w-[300px] bg-white border-none rounded-2xl p-3"
+        className="min-w-[250px] bg-white border-none rounded-3xl p-3 shadow-0"
         align="center"
       >
         {languages.map((language) => (
