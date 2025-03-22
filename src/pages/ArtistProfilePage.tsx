0d47c8debc514bb30683db7e398eebdee0541ece
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-03-22 21:11:24 +0100

    s

--- src/pages/ArtistProfilePage.tsx
+++ src/pages/ArtistProfilePage.tsx
@@ -423,7 +423,7 @@ const ArtistProfilePage = () => {
             
             {/* Right Sticky Content */}
             <div className="lg:sticky lg:top-24 h-fit bg-white dark:bg-vyba-dark-bg rounded-3xl px-6 py-4">
-              <div className="flex items-center gap-2 mb-3">
+              <div className="flex items-center gap-2 mb-2">
                 <p className="text-base text-neutral-600 dark:text-neutral-300">{artist.location} · {artist.availability}</p>
               </div>
               
