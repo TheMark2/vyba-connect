f181753d3ce4bf44035de61767c231ddf7b336a7
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-03-22 13:30:40 +0100

    s

--- src/pages/ArtistsPage.tsx
+++ src/pages/ArtistsPage.tsx
@@ -161,7 +161,7 @@ const ArtistsPage = () => {
             ? "grid grid-cols-1 gap-8"
             : isMobile
               ? "grid grid-cols-2 gap-6"
-              : "grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 gap-8"
+              : "grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-5 2xl:grid-cols-5 gap-8"
           }
         `}>
           {artists.map(artist => (
