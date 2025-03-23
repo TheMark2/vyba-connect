da79ebfe8602199da7077c72a299138a89c54a6f
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-03-23 20:59:47 +0100

    s

--- src/pages/ArtistProfilePage.tsx
+++ src/pages/ArtistProfilePage.tsx
@@ -316,12 +316,12 @@ const ArtistProfilePage = () => {
                     className="w-full"
                   >
                     <CollapsibleTrigger asChild>
-                      <button className="flex items-center justify-between w-full p-8 text-xl bg-secondary font-bold hover:bg-secondary/80 dark:bg-vyba-dark-secondary/70 dark:hover:bg-vyba-dark-secondary/90 transition-colors duration-200">
+                      <button className="flex items-center justify-between w-full p-4 text-xl bg-secondary font-bold hover:bg-secondary/80 dark:bg-vyba-dark-secondary/70 dark:hover:bg-vyba-dark-secondary/90 transition-colors duration-200">
                         <span>Experiencia, Repertorio, Logística y Equipamiento, Tiempos</span>
                         <ChevronDown className={`h-6 w-6 transition-transform duration-500 ${isInfoOpen ? "rotate-180" : ""}`} />
                       </button>
                     </CollapsibleTrigger>
-                    <CollapsibleContent className="p-8 pt-0 space-y-8 bg-secondary rounded-2xl rounded-tr-none rounded-tl-none dark:bg-vyba-dark-secondary/70">
+                    <CollapsibleContent className="p-8 pt-0 space-y-8 bg-secondary rounded-xl rounded-tr-none rounded-tl-none dark:bg-vyba-dark-secondary/70">
                       {/* Experiencia */}
                       <div>
                         <h3 className="text-xl font-bold mb-3 flex items-center">
