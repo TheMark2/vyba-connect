05781b15b5b40d90aa2fd71b4982e6752609b96d
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-03-29 17:19:02 +0100

    s

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -301,9 +301,6 @@ const AuthPage = () => {
                   </>
                 ) : registerStep === 2 ? (
                   <div className="space-y-6">
-                    <div className="text-center mb-8">
-                      <h2 className="text-4xl font-bold dark:text-white">¿Cómo quieres usar VYBA?</h2>
-                    </div>
                     
                     <form onSubmit={handleRegisterSubmit} className="space-y-8">
                       <RadioGroup 
