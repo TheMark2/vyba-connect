e12f884bde5665e8645527c7917c238d01c29e46
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-03-31 19:31:48 +0200

    s

--- src/pages/ArtistThankYouPage.tsx
+++ src/pages/ArtistThankYouPage.tsx
@@ -111,8 +111,8 @@ const ArtistThankYouPage = () => {
                 <div className="space-y-4">
                   <div className="bg-gray-50 dark:bg-vyba-dark-secondary/80 p-4 rounded-2xl transition-all duration-300">
                     <div className="flex gap-6 items-center justify-center mb-4">
-                      <GraduationCap size={48} className="text-black dark:text-white mb-3" />
-                      <h3 className="text-5xl font-bold text-center text-black dark:text-white">
+                      <GraduationCap size={48} className="text-black dark:text-white" />
+                      <h3 className="text-5xl font-black text-center text-black dark:text-white">
                         {artistInfo.artistName}
                       </h3>
                     </div>
