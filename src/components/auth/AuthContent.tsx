279d7ba453a0dc25ca2b91839822ae216bf57108
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-04-01 21:31:44 +0200

    s

--- src/components/auth/AuthContent.tsx
+++ src/components/auth/AuthContent.tsx
@@ -70,7 +70,7 @@ const AuthContent: React.FC<AuthContentProps> = ({
   return (
     <div className={`w-full max-w-screen min-h-screen bg-[#f9f7f3] dark:bg-vyba-dark-bg ${isMobile ? 'py-8' : 'py-0'}`}>
       <div className="max-w-md mx-auto px-6 md:px-0 pt-10">
-        <h1 className="text-6xl md:text-4xl font-bold text-center mb-2">
+        <h1 className="text-4xl md:text-6xl font-black text-center mb-2">
           Bienvenido/a a VYBA
         </h1>
         <p className="text-2xl md:text-4xl text-center mb-8">
