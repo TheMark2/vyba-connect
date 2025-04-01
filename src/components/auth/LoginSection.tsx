6de7fd7462d2edd6f19447c31fb34b5f75e0041d
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-04-01 21:32:54 +0200

    s

--- src/components/auth/LoginSection.tsx
+++ src/components/auth/LoginSection.tsx
@@ -39,7 +39,7 @@ const LoginSection: React.FC<LoginSectionProps> = ({
       
       <Button 
         type="button" 
-        variant="outline" 
+        variant="secondary" 
         className="w-full flex items-center justify-center gap-2 bg-white text-black hover:bg-gray-50 h-12 rounded-full" 
         onClick={handleLoginSubmit}
       >
