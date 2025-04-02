f8f95224f55fdf172c8afe0e2eedeeaf7f7d681a
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-04-02 18:25:56 +0200

    s

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -11,7 +11,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
         <input
           {...props}
           className={cn(
-            "flex h-14 w-full rounded-lg bg-[#F7F7F7] px-5 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground focus-visible:outline-none focus:ring-[#666666] focus:ring-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm text-black font-medium placeholder:font-medium placeholder:text-muted-foreground dark:text-white dark:bg-[#F7F7F7] dark:placeholder:text-gray-400",
+            "flex h-14 w-full rounded-lg bg-[#F7F7F7] px-5 py-2 text-sm file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground focus-visible:outline-none focus:ring-[#666666] focus:ring-4 transition-all duration-300 ease-in-out disabled:cursor-not-allowed disabled:opacity-50 md:text-sm text-black font-medium placeholder:font-medium placeholder:text-muted-foreground dark:text-white dark:bg-[#F7F7F7] dark:placeholder:text-gray-400",
             className
           )}
           ref={combinedRef}
