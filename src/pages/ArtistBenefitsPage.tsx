75b5daa71059bb55de857c3e0ba595a00bdc3154
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-04-04 09:05:13 +0200

    s

--- src/pages/ArtistBenefitsPage.tsx
+++ src/pages/ArtistBenefitsPage.tsx
@@ -31,7 +31,7 @@ const ArtistBenefitsPage = () => {
                   </div>
                 </div>
                 
-                <div className="flex-[0.6] relative">
+                <div className="flex-[0.8] relative">
                   <div className="flex justify-center relative h-[500px] overflow-hidden">
                     <img src="/lovable-uploads/a02627ff-e1d7-4640-959e-74140c8af909.png" alt="VYBA app en iPhone" className="absolute bottom-[-50%] h-[700px] object-contain" />
                   </div>
