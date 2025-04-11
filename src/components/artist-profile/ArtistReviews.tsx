4bf92dc80c87e1e8bfaa31ba1c9b51de1514642e
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-04-12 01:52:15 +0200

    s

--- src/components/artist-profile/ArtistReviews.tsx
+++ src/components/artist-profile/ArtistReviews.tsx
@@ -60,7 +60,7 @@ const ReviewItem = ({
           </Button>
         </div>
         
-        <div className="flex flex-col md:flex-row gap-6">
+        <div className="flex flex-col md:flex-row gap-12">
           <div className="md:w-1/4">
             <div className="flex items-center mb-2">
               <Avatar className="h-16 w-16 rounded-lg mr-3">
