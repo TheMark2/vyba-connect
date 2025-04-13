b36444ee520b1f092c3790fbcaa62149e31c7e60
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-04-13 18:47:45 +0200

    s

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -27,12 +27,12 @@ const DrawerContent = React.forwardRef<React.ElementRef<typeof DrawerPrimitive.C
   <DrawerPrimitive.Content 
     ref={ref} 
     className={cn(
-      "fixed inset-x-0 bottom-0 z-50 flex h-auto flex-col rounded-t-[10px] border bg-background pt-12", // Added pt-12 to push content down
+      "fixed inset-x-0 bottom-0 z-50 flex h-auto flex-col rounded-t-[10px] border bg-background mt-12", // Added pt-12 to push content down
       className
     )} 
     {...props}
   >
-    <DrawerPrimitive.Close className="absolute top-4 right-4 z-50 rounded-full p-2 hover:bg-muted/20"> {/* Changed left-4 to right-4 */}
+    <DrawerPrimitive.Close className="absolute top-4 left-4 z-50 rounded-full p-2 hover:bg-muted/20"> {/* Changed left-4 to right-4 */}
       <X className="h-6 w-6" />
       <span className="sr-only">Close</span>
     </DrawerPrimitive.Close>
