d3d665cc9d097404a2b744daeb0ced6461c6c1d2
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-04-14 19:48:28 +0200

    s

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -33,29 +33,32 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border border-stone-200 bg-white p-6 pt-14 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg dark:border-stone-800 dark:bg-stone-950",
+        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border border-stone-200 bg-white p-6 pt-14 shadow-lg duration-200",
         className
       )}
       {...props}
     >
-      <DialogPrimitive.Close
-        className="absolute top-4 left-4 p-2 rounded-full hover:bg-black/5 dark:hover:bg-white/10 transition"
-      >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5 text-black dark:text-white"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
+      {/* Contenedor del botón */}
+      <div className="absolute top-4 left-4 z-50">
+        <DialogPrimitive.Close
+          className="p-2 rounded-full hover:bg-black/5 dark:hover:bg-white/10 transition"
         >
-          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
-        </svg>
-        <span className="sr-only">Cerrar</span>
-      </DialogPrimitive.Close>
-
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5 text-black dark:text-white"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+          </svg>
+          <span className="sr-only">Cerrar</span>
+        </DialogPrimitive.Close>
+      </div>
 
       {children}
     </DialogPrimitive.Content>
+
   </DialogPortal>
 ))
 DialogContent.displayName = DialogPrimitive.Content.displayName
