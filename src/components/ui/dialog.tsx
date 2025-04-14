b8aa3ba9ac8013206abdc6e5860f98b5b9566f5b
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-04-14 22:14:29 +0200

    s

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -38,7 +38,7 @@ const DialogContent = React.forwardRef<
       )}
       {...props}
     >
-      <div className="sticky top-0 z-10 p-4 bg-white sm:rounded-t-3xl">
+      <div className="sticky top-0 z-10 p-2 bg-white sm:rounded-t-3xl">
         <DialogPrimitive.Close className="hover:text-gray-500 transition-colors hover:bg-gray-100/80 p-2 rounded-full">
           <X className="h-5 w-5" />
           <span className="sr-only">Cerrar</span>
