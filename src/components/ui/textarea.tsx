a08d14397664126e978fa21fa0fbdf596ab0d9e7
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-04-14 22:23:43 +0200

    s

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -1,6 +1,4 @@
-
 import * as React from "react"
-
 import { cn } from "@/lib/utils"
 
 export interface TextareaProps
@@ -11,7 +9,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     return (
       <textarea
         className={cn(
-          "flex min-h-[80px] w-full rounded-3xl bg-[#F7F7F7] px-6 py-4 text-base md:text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-0 focus:ring-2 focus:ring-black dark:focus:ring-white transition-all duration-200 ease-in-out disabled:cursor-not-allowed disabled:opacity-50 text-black font-medium placeholder:font-medium dark:text-white dark:bg-vyba-dark-secondary/20 dark:placeholder:text-gray-400 hover:bg-[#F0F0F0] dark:hover:bg-vyba-dark-secondary/30",
+          "flex min-h-[80px] w-full rounded-3xl bg-[#F7F7F7] px-6 py-4 text-base md:text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-0 focus:ring-2 focus:ring-black dark:focus:ring-white transition-all duration-300 ease-in-out disabled:cursor-not-allowed disabled:opacity-50 text-black font-medium placeholder:font-medium dark:text-white dark:bg-vyba-dark-secondary/20 dark:placeholder:text-gray-400 hover:bg-[#F0F0F0] dark:hover:bg-vyba-dark-secondary/30",
           className
         )}
         ref={ref}
@@ -20,6 +18,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
     )
   }
 )
+
 Textarea.displayName = "Textarea"
 
 export { Textarea }
