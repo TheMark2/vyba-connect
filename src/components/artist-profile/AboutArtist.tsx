3e695a76c541ab145841afee4c8a799031dea29d
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-04-15 00:11:47 +0200

    s

--- src/components/artist-profile/AboutArtist.tsx
+++ src/components/artist-profile/AboutArtist.tsx
@@ -13,7 +13,7 @@ const AboutArtist = forwardRef<HTMLDivElement, AboutArtistProps>(
     return (
       <div className="mb-10" ref={ref}>
         <h2 className="text-3xl font-semibold mb-6">Sobre mi</h2>
-        <p className="text-base mb-5 leading-relaxed">
+        <p className="text-base mb-5 font-light leading-relaxed">
           {description}
         </p>
         
