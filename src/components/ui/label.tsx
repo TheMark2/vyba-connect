191940b322f3fb1a67aebc7bce0f000f2209479f
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-04-14 22:30:03 +0200

    s

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,24 +1,24 @@
 import * as React from "react"
-import * as LabelPrimitive from "@radix-ui/react-label"
-import { cva, type VariantProps } from "class-variance-authority"
-
 import { cn } from "@/lib/utils"
 
-const labelVariants = cva(
-  "text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...props }, ref) => {
+    return (
+      <input
+        className={cn(
+          "flex h-12 w-full rounded-xl bg-[#F7F7F7] px-6 py-2 text-base md:text-sm ring-offset-background placeholder:text-muted-foreground placeholder:font-normal focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-0 focus:ring-2 focus:ring-black dark:focus:ring-white transition-all duration-300 ease-in-out disabled:cursor-not-allowed disabled:opacity-50 text-black font-medium dark:text-white dark:bg-vyba-dark-secondary/20 dark:placeholder:text-gray-400 hover:bg-[#F0F0F0] dark:hover:bg-vyba-dark-secondary/30",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
 )
 
-const Label = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
-    VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root
-    ref={ref}
-    className={cn(labelVariants(), className)}
-    {...props}
-  />
-))
-Label.displayName = LabelPrimitive.Root.displayName
+Input.displayName = "Input"
 
-export { Label }
+export { Input }
