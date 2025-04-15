86736b5a5f1772bddcdae4218a6def900bb6bb60
TheMark2 <87651166+TheMark2@users.noreply.github.com>
2025-04-15 16:50:03 +0200

    s

--- src/components/ArtistProfileCard.tsx
+++ src/components/ArtistProfileCard.tsx
@@ -316,14 +316,14 @@ const ArtistProfileCard = ({
         ) : (
           <div className="pt-3 flex flex-col gap-1 bg-transparent">
             <div className="flex justify-between items-center">
-              <h3 className="text-base font-bold">{name}</h3>
+              <h3 className="text-base font-medium">{name}</h3>
               <div className="flex items-center gap-1">
-                <Star className="h-4 w-4 fill-black stroke-black dark:fill-white dark:stroke-white" />
-                <span className="text-base font-bold">{rating.toFixed(1)}</span>
+                <Star className="h-3 w-3 fill-black stroke-black dark:fill-white dark:stroke-white" />
+                <span className="text-base font-medium">{rating.toFixed(1)}</span>
               </div>
             </div>
-            <p className="text-sm text-gray-500 dark:text-gray-400 line-clamp-1 font-light">{description}</p>
-            <p className="text-sm font-bold">
+            <p className="text-base text-gray-500 dark:text-gray-400 line-clamp-1 font-regular">{description}</p>
+            <p className="text-base font-medium">
               {priceRange}
             </p>
           </div>
