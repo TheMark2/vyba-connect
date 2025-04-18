df0cc309a3d8ae1d30f33df2ab8e4de991f93796
Marc Vendrell Pedragosa <marc@MacBook-Air-de-Marc-4.local>
2025-04-18 21:19:00 +0200

    2

--- src/pages/dashboard/Overview.tsx
+++ src/pages/dashboard/Overview.tsx
@@ -1,10 +1,9 @@
-
 import React, { useState } from 'react';
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { Bell, Mail, User, Settings, BarChart } from "lucide-react";
-import { CircleAlert } from "lucide-react";
+import { Bell, Mail, User, Settings, BarChart, CalendarClock, MessageSquare, Clock, Lightbulb, Star, CircleAlert } from "lucide-react";
 import { Progress } from "@/components/ui/progress";
+import { Carousel, CarouselItem } from "@/components/ui/carousel";
 
 const Overview = () => {
   const [userName] = useState<string>("Usuario");
@@ -19,13 +18,13 @@ const Overview = () => {
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 px-32">
         <div className="bg-vyba-gray rounded-2xl p-6 relative">
-          <h2 className="text-2xl font-medium mb-1">Verifica tu identidad</h2>
+          <h2 className="text-xl font-medium mb-1">Verifica tu identidad</h2>
           <p className="text-muted-foreground text-xs text-red-500">
             Requerido para publicar tu perfil
           </p>
           
           <div className="flex items-center w-full mt-4">
-            <Button variant="link" className="p-0 text-vyba-tertiary text-base text-vyba-navy underline rounded-none">
+            <Button variant="link" className="p-0 text-vyba-tertiary text-sm text-vyba-navy underline rounded-none">
               Verificar ahora
             </Button>
           </div>
@@ -35,17 +34,17 @@ const Overview = () => {
         <div className="bg-vyba-gray rounded-2xl p-6 relative">
           <div className="flex justify-between items-center">
             <div className="flex-1 pr-4">
-              <h2 className="text-2xl font-medium mb-1">Progreso</h2>
-              <p className="text-muted-foreground text-sm text-vyba-tertiary text-light mb-0">
+              <h2 className="text-xl font-medium mb-1">Progreso</h2>
+              <p className="text-muted-foreground text-sm text-vyba-tertiary font-light mb-0">
                 Con un 100% hay mas probabilidades de recibir mas solicitudes
               </p>
             </div>
-            <div className="relative w-16 h-16">
+            <div className="relative w-32 h-32">
               <div className="absolute inset-0 flex items-center justify-center">
                 <div className="absolute inset-0">
                   <svg className="w-full h-full" viewBox="0 0 100 100">
                     <circle
-                      className="text-gray-200 stroke-current"
+                      className="text-vyba-tertiary/20 stroke-current"
                       strokeWidth="10"
                       cx="50"
                       cy="50"
@@ -53,7 +52,7 @@ const Overview = () => {
                       fill="none"
                     />
                     <circle
-                      className="text-blue-500 stroke-current"
+                      className="text-green-500 stroke-current"
                       strokeWidth="10"
                       strokeLinecap="round"
                       cx="50"
@@ -75,7 +74,116 @@ const Overview = () => {
             </div>
           </div>
         </div>
+
+        <div className="bg-vyba-gray rounded-2xl p-6 relative">
+          <div className="flex-1 pr-12">
+            <h2 className="text-xl font-medium mb-1">Eventos próximos</h2>
+            <p className="text-muted-foreground text-sm text-vyba-tertiary pr-12 font-light">
+              Tienes 3 eventos programados para este mes
+            </p>
+            
+            <div className="flex items-center w-full mt-4">
+              <Button variant="link" className="p-0 text-vyba-tertiary text-sm text-vyba-navy underline rounded-none">
+                Ver calendario
+              </Button>
+            </div>
+          </div>
+          <CalendarClock className="absolute top-1/2 right-6 -translate-y-1/2 h-9 w-9 text-vyba-navy stroke-[1.5]" />
+        </div>
+
+        <div className="bg-vyba-gray rounded-2xl p-6 relative">
+          <div className="flex-1 pr-12">
+            <h2 className="text-xl font-medium mb-1">Mensajes nuevos</h2>
+            <p className="text-muted-foreground text-sm text-vyba-tertiary pr-12 font-light">
+              Tienes 5 mensajes sin leer de clientes potenciales
+            </p>
+            
+            <div className="flex items-center w-full mt-4">
+              <Button variant="link" className="p-0 text-vyba-tertiary text-sm text-vyba-navy underline rounded-none">
+                Revisar mensajes
+              </Button>
+            </div>
+          </div>
+          <MessageSquare className="absolute top-1/2 right-6 -translate-y-1/2 h-9 w-9 text-vyba-navy" />
+        </div>
+
+        <div className="bg-vyba-gray rounded-2xl p-6 relative">
+          <div className="flex-1 pr-12">
+            <h2 className="text-xl font-medium mb-1">Recordatorios</h2>
+            <p className="text-muted-foreground text-sm text-vyba-tertiary pr-12 font-light">
+              Apúntate todos los eventos en el calendario para así no olvidarte
+            </p>
+            
+            <div className="flex items-center w-full mt-4">
+              <Button variant="link" className="p-0 text-vyba-tertiary text-sm text-vyba-navy underline rounded-none">
+                Ver todos
+              </Button>
+            </div>
+          </div>
+          <Clock className="absolute top-1/2 right-6 -translate-y-1/2 h-9 w-9 text-vyba-navy" />
+        </div>
+
+        <div className="bg-vyba-gray rounded-2xl p-6 relative">
+          <div className="flex-1 pr-12">
+            <h2 className="text-xl font-medium mb-1">Consejos</h2>
+            <p className="text-muted-foreground text-sm text-vyba-tertiary pr-12 font-light">
+              Responde a los mensajes lo más rápido posible para así ofrecer una mejor experiencia a quienes te escriben.
+            </p>
+            
+            <div className="flex items-center w-full mt-4">
+              <Button variant="link" className="p-0 text-vyba-tertiary text-sm text-vyba-navy underline rounded-none">
+                Más consejos
+              </Button>
+            </div>
+          </div>
+          <Lightbulb className="absolute top-1/2 right-6 -translate-y-1/2 h-9 w-9 text-vyba-navy" />
+        </div>
+
+        <div className="bg-vyba-gray rounded-2xl p-6 relative">
+          <div className="flex-1 pr-12">
+            <h2 className="text-xl font-medium mb-1">Novedades</h2>
+            <p className="text-muted-foreground text-sm text-vyba-tertiary pr-12 font-light">
+              Nuevas funciones disponibles para mejorar tu visibilidad
+            </p>
+            
+            <div className="flex items-center w-full mt-4">
+              <Button variant="link" className="p-0 text-vyba-tertiary text-sm text-vyba-navy underline rounded-none">
+                Explorar
+              </Button>
+            </div>
+          </div>
+          <Star className="absolute top-1/2 right-6 -translate-y-1/2 h-9 w-9 text-vyba-navy" />
+        </div>
       </div>
+      <section className="px-32 mt-24 relative overflow-hidden">
+        <div 
+          className="absolute inset-0 bg-cover bg-center" 
+          style={{ backgroundImage: "url('/lovable-uploads/dashboardbannersteps.jpg')" }}
+        ></div>
+        <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+        <div className="relative z-10 p-8 text-white h-full flex flex-col justify-center mt-32">
+          <h2 className="text-5xl font-semibold text-white mb-4">Pasos esenciales</h2>
+          <p className="text-lg mb-6 text-white/80 font-light">Completa tu perfil para aumentar tus oportunidades de recibir más solicitudes</p>
+          <Button className="w-max bg-vyba-navy hover:bg-vyba-navy/90">Completar perfil</Button>
+        </div>
+        <div>
+          <Carousel className="w-full">
+            <CarouselItem className="w-64 h-64 bg-vyba-gray rounded-2xl">
+              <div className="flex flex-col justify-between p-6">
+                <Star className="h-12 w-12 text-vyba-navy mb-2" />
+                <div className="flex flex-col">
+                  <div className="flex items-center">
+                    <CardTitle className="text-2xl font-medium">Experiencia</CardTitle>
+                  </div>
+                  <CardDescription className="text-sm font-light">
+                    Añade testimonios de clientes anteriores para aumentar tu reputación
+                  </CardDescription>
+                </div>
+              </div>
+            </CarouselItem>
+          </Carousel>
+        </div>
+      </section>
     </div>
   );
 };
